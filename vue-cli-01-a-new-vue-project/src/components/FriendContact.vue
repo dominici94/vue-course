bd522fe8914d1649c2fb37d9dead10d1742aa8d0
dominici94 <mattiadominici94@gmail.com>
2023-03-17 10:56:53 +0100

    props

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ favoriteContact ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+// import { defineComponent } from "@vue/composition-api";
+
+export default {
+  // props: ["name", "phoneNumber", "emailAddress"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator: function (value) {
+        return value === true || value === false;
+      },
+    },
+  },
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      favoriteContact: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.favoriteContact = !this.favoriteContact;
+    },
+  },
+};
+</script>
