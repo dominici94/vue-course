74578c746c8ad94763d5addd7762a04e77e0fcb2
dominici94 <mattiadominici94@gmail.com>
2023-03-19 18:52:57 +0100

    slot feature

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <!-- <base-card> -->
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <!-- </base-card> -->
+    <course-goal>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps["anotherProp"] }}</p>
+      </template>
+    </course-goal>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+// import BaseCard from "./components/BaseCard.vue";
+import CourseGoal from "./components/CourseGoal.vue";
+
+export default {
+  components: {
+    "the-header": TheHeader,
+    "badge-list": BadgeList,
+    "user-info": UserInfo,
+    CourseGoal,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
