53eda9a7a065da3c0d76749211f845d19c132b89
dominici94 <mattiadominici94@gmail.com>
2023-03-24 01:14:52 +0100

    finish vuex theory

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -1,48 +1,7 @@
 import { createApp } from 'vue';
-import { createStore } from 'vuex';
 
 import App from './App.vue';
-
-const store = createStore({
-  state() {
-    return {
-      counter: 0,
-    };
-  },
-  mutations: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, payload) {
-      state.counter = state.counter + payload.value;
-    },
-  },
-  actions: {
-    increment(context) {
-      setTimeout(function () {
-        context.commit('increment');
-      }, 2000);
-    },
-    increase(context, payload) {
-      context.commit('increase', payload);
-    },
-  },
-  getters: {
-    finalCounter(state) {
-      return state.counter * 3;
-    },
-    normalizedCounter(_, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) {
-        return 0;
-      }
-      if (finalCounter > 100) {
-        return 100;
-      }
-      return finalCounter;
-    },
-  },
-});
+import store from './store/index';
 
 const app = createApp(App);
 
