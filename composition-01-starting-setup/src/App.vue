b520e08bf05a73aba4c9b5c5f4df554e377d7149
dominici94 <mattiadominici94@gmail.com>
2023-03-31 11:27:38 +0200

    start composition API

---
+++ composition-01-starting-setup/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+  </section>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    // const uName = ref('Maximilian');
+    // // const uName = 'Maximilian';
+    // const uAge = ref(31);
+    // const user = ref({
+    //   name: 'Maximilian',
+    //   age: 31,
+    // });
+    const user = reactive({
+      name: 'Maximilian',
+      age: 31,
+    });
+
+    // setTimeout(function () {
+    //   uName.value = 'Max';
+    //   uAge.value = 32;
+    // }, 2000);
+
+    setTimeout(function () {
+      // user.value.name = 'Max';
+      // user.value.age = 32;
+      user.name = 'Max';
+      user.age = 32;
+    }, 2000);
+
+    return {
+      // userName: user.value.name,
+      // age: user.value.age,
+      user: user,
+    };
+  },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
